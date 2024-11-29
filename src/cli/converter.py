c6c8e8d5d2ac5f7ce3257115f13eb378d2161d0d
rrodero <rrodero@live.com>
2024-11-29 19:21:59 -0300

    Improving Code Clarity and Maintainability

--- src/cli/converter.py
+++ src/cli/converter.py
@@ -1,21 +1,36 @@
 import json
-
-
-def load_alphabet() -> dict[str, str]:
-    with open("../../resources/morse.json") as morse_json:
-        return json.load(morse_json)
+from typing import Dict
 
 
 class Converter:
+    """A class for converting text to Morse code and vice versa."""
 
     def __init__(self):
-        self.morse_alphabet = load_alphabet()
+        """Initializes the Converter with the Morse alphabet."""
+        with open("../../resources/morse.json") as morse_json:
+            self.morse_alphabet: Dict[str, str] = json.load(morse_json)
 
     def encrypt_message(self, message: str) -> str:
+        """Encrypts a given text message into Morse code.
+
+            Args:
+                message: The text message to encrypt.
+
+            Returns:
+                The encrypted Morse code.
+        """
         ciphers = [self.morse_alphabet[letter] + " " if letter != " " else " " for letter in message]
         return "".join(ciphers)
 
     def decrypt_message(self, morse_code: str) -> str:
+        """Decrypts a given Morse code into text.
+
+            Args:
+                morse_code: The Morse code to decrypt.
+
+            Returns:
+                The decrypted text.
+        """
         morse_code = morse_code.replace("  ", " / ")
         print(morse_code.split())
         clear_letters = [list(self.morse_alphabet.keys())[list(self.morse_alphabet.values()).index(letter)]
--- src/cli/main.py
+++ src/cli/main.py
@@ -8,69 +8,65 @@ app = typer.Typer(rich_markup_mode="rich", no_args_is_help=True)
 
 converter = Converter()
 
+
+def process_file(file_path: str, process_func):
+    """Processes the content of a file line by line.
+
+        Args:
+            file_path: The path to the file.
+            process_func: The function to apply to each line.
+
+        Returns:
+            The processed text.
+    """
+    try:
+        with open(file_path, "r") as file:
+            return "".join(process_func(line) for line in file)
+    except FileNotFoundError:
+        print(f"Input file '{file_path}' not found.")
+        raise typer.Exit(code=1)
+
+
 @app.command()
 def encrypt(text: Annotated[Optional[str], typer.Argument(help="Text to be encrypted to morse code")] = None,
             input_file: Annotated[str, typer.Option("--input", "-i", help="Specify the input file")] = None,
             output_file: Annotated[str, typer.Option("--output", "-o", help="Specify the output file")] = None):
 
-    morse_code = ""
+    """Encrypts the given text or file content to Morse code."""
+
     if text:
         morse_code = converter.encrypt_message(text)
     elif input_file:
-        try:
-            with open(input_file, "r") as input_file:
-                for line in input_file:
-                    morse_code += converter.encrypt_message(line)
-        except FileNotFoundError:
-            print("Input file was not found")
-            raise typer.Exit(code=1)
+        morse_code = process_file(input_file, converter.encrypt_message)
     else:
-        print("No provided message or input file to encrypt")
+        print("No text or input file provided for encryption.")
         raise typer.Abort()
 
     if output_file:
-        try:
-            with open(output_file, "w") as output_file:
-                output_file.write(morse_code)
-        except FileNotFoundError:
-            print("Output file was not found")
-            raise typer.Exit(code=1)
+        with open(output_file, "w") as output_file:
+            output_file.write(morse_code)
     else:
         print(morse_code)
 
 
-
 @app.command()
-def decrypt(morse_code: Annotated[Optional[str], typer.Argument()] = None,
+def decrypt(morse_code: Annotated[Optional[str], typer.Argument(help="Morse code to be decrypted to clear text")] = None,
             input_file: Annotated[str, typer.Option("--input", "-i", help="Specify the input file")] = None,
             output_file: Annotated[str, typer.Option("--output", "-o", help="Specify the output file")] = None):
-
-    text = ""
+    """Decrypts the given Morse code or file content to text."""
     if morse_code:
         text = converter.decrypt_message(morse_code)
     elif input_file:
-        try:
-            with open(input_file, "r") as input_file:
-                for line in input_file:
-                    text += converter.decrypt_message(line)
-        except FileNotFoundError:
-            print("Input file was not found")
-            raise typer.Exit(code=1)
+        text = process_file(input_file, converter.decrypt_message)
     else:
-        print("No provided morse code or input file to decrypt")
+        print("No Morse code or input file provided for decryption.")
         raise typer.Abort()
 
     if output_file:
-        try:
-            with open(output_file, "w") as output_file:
-                output_file.write(text)
-        except FileNotFoundError:
-            print("Output file was not found")
-            raise typer.Exit(code=1)
+        with open(output_file, "w") as output_file:
+            output_file.write(text)
     else:
         print(text)
-
-
 
 
 if __name__ == "__main__":
